695f4e4d1da9af718987b2444650e83e1d9892ce
function07 <houssine199222@gmail.com>
2022-12-29 21:51:42 -0500

    Add a container div in index.html and a function in script.js to loop through the myLibrary array and display it in the browser

--- script.js
+++ script.js
@@ -5,6 +5,29 @@ function Book(title, author, pages, isRead) {
   this.author = author;
   this.pages = pages;
   this.isRead = isRead;
+  this.info = function () {
+    return `${this.title} by ${this.author} ,${this.pages} ,${this.isRead}`;
+  };
 }
 
-function addBookToLibrary() {}
+const novel = new Book("the Hobbit", "J.R.R. Tolkien", "295 pages", "not read");
+
+function addBookToLibrary() {
+  return myLibrary.push(novel);
+}
+
+function loopThroughLibrary() {
+  const displayBook = myLibrary.map((book) => book.info());
+
+  myLibrary.forEach((e) => e.info());
+
+  const container = document.querySelector(".container");
+
+  const content = document.createElement("div");
+  content.classList.add("content");
+  content.textContent = `${displayBook}`;
+
+  container.appendChild(content);
+
+  myLibrary = [];
+}
