520c70b7025d6538e0e279b106bc85bacd63cc68
function07 <houssine199222@gmail.com>
2022-12-31 00:17:32 -0500

    Add an event listener to submitButton that will store input values as arguments for new Book()

--- script.js
+++ script.js
@@ -10,25 +10,39 @@ function Book(title, author, pages, isRead) {
   };
 }
 
-function addBookToLibrary() {
-  return myLibrary.push();
+function addBookToLibrary(book) {
+  return myLibrary.push(book);
 }
 
 function loopThroughLibrary() {
   const displayBook = myLibrary.map((book) => book.info());
 
-  myLibrary.forEach((e) => e.info());
-
   const container = document.querySelector(".cardContainer");
 
   const content = document.createElement("div");
   content.classList.add("content");
-  content.textContent = `${displayBook}`;
 
+  content.textContent = displayBook[0].toString();
   container.appendChild(content);
 
   myLibrary = [];
 }
+
+// Save inputs as arguments for new Book constructor
+const submitButton = document.querySelector("#submitButton");
+
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const bookTitle = document.querySelector("#bookTitle").value;
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector("#pages").value;
+  const isRead = document.querySelector("#isRead").value;
+
+  const book = new Book(bookTitle, author, pages, isRead);
+  addBookToLibrary(book);
+  loopThroughLibrary();
+});
 
 // Coding for modal
 const modal = document.querySelector(".modal");
