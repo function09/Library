a6031a3925fcd0589ddab5c059e90926772bdcb2
function07 <houssine199222@gmail.com>
2023-01-07 23:19:56 -0500

    Clean up code and add function that removes book from display

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const addBookButton = document.querySelector("#addBookButton");
+
 let myLibrary = [];
 
 function Book(title, author, pages, isRead) {
@@ -15,34 +16,70 @@ function addBookToLibrary(book) {
   return myLibrary.push(book);
 }
 
-// Displays book info as a card
+// Assigns an array element data attribute to remove book button
+function assignDataValues() {
+  const selectRemoveBookButton = document.querySelectorAll(".removeBook");
+
+  let arrayValue = 0;
+  let indexValue = 0;
+  selectRemoveBookButton.forEach((button) => {
+    if (arrayValue === 0 || indexValue === 0) {
+      button.dataset.value = "array-0";
+      button.dataset.index = 0;
+      arrayValue += 1;
+      indexValue += 1;
+    } else {
+      button.dataset.value = `array-${arrayValue++}`;
+      button.dataset.index = indexValue++;
+    }
+  });
+}
+
+// Assigns an ID to content
+function assignId() {
+  const selectContent = document.querySelectorAll(".content");
+
+  let elementValue = 0;
+
+  selectContent.forEach((content) => {
+    if (elementValue === 0) {
+      content.setAttribute("id", `array-${elementValue}`);
+      elementValue += 1;
+    } else {
+      content.setAttribute("id", `array-${elementValue++}`);
+    }
+  });
+}
+
+// Displays book info as a card with a remove book button
 function createCardDisplay() {
   const displayBook = myLibrary.map((book) => book.info());
 
   const selectContainer = document.querySelector(".cardContainer");
-
   const content = document.createElement("div");
   content.classList.add("content");
-
   content.textContent = displayBook[myLibrary.length - 1].toString();
   selectContainer.appendChild(content);
-}
 
-// Assigns an array element data attribute
-function addDataValues() {
-  const selectCard = document.querySelectorAll(".content");
+  const removeBookButton = document.createElement("button");
+  removeBookButton.textContent = "Remove book";
+  removeBookButton.classList.add("removeBook");
+  content.appendChild(removeBookButton);
 
-  let elementValue = 0;
-
-  selectCard.forEach((card) => {
-    if (elementValue === 0) {
-      card.dataset.arrayElement = "0";
-      elementValue += 1;
-    } else {
-      card.dataset.arrayElement = elementValue++;
+  removeBookButton.addEventListener("click", (e) => {
+    const selectArrayValue = e.target.dataset.value;
+    const selectIndex = e.target.dataset.index;
+    const selectId = e.target.parentElement.id;
+    if (selectArrayValue === selectId) {
+      const getContent = document.getElementById(`${selectId}`);
+      getContent.remove();
+      myLibrary.splice(selectIndex, 1);
+      assignId();
+      assignDataValues();
     }
   });
 }
+
 // Obtains values from the respective inputs and stores them as arguments in the new Book() object before creating a card display of the book
 addBookButton.addEventListener("click", (e) => {
   const bookTitle = document.querySelector("#bookTitle").value;
@@ -54,7 +91,8 @@ addBookButton.addEventListener("click", (e) => {
 
   addBookToLibrary(book);
   createCardDisplay();
-  addDataValues();
+  assignId();
+  assignDataValues();
   e.preventDefault();
 });
 
